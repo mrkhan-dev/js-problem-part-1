d7b603d5fb401f00ae698f97a40ecac781c30cf6
mrkhan-dev <sk5094625@gmail.com>
2024-02-04 18:45:19 +0600

    added some code

--- oddAverage.js
+++ oddAverage.js
@@ -14,6 +14,26 @@ function oddAverage(numbers) {
   return avg;
 }
 
+// const numbers = [10, 15, 60, 87, 41, 36, 97];
+// const avg = oddAverage(numbers);
+// console.log(avg);
+
+function evenAverage(numbers) {
+  const even = [];
+  for (const number of numbers) {
+    if (number % 2 === 0) {
+      even.push(number);
+    }
+  }
+  let some = 0;
+  for (const number of even) {
+    some += number;
+  }
+  const count = even.length;
+  const avg = some / count;
+  return avg;
+}
+
 const numbers = [10, 15, 60, 87, 41, 36, 97];
-const avg = oddAverage(numbers);
+const avg = evenAverage(numbers);
 console.log(avg);
